ed38251f2fc91a8c2ead93d5f6f67737a47e17e7
Aravind <aravindpanil@gmail.com>
2017-12-20 21:39:49 +0530

    Hello World Test

---
+++ Java_Final_Tomcat/src/Hello.java
@@ -0,0 +1,28 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter obj = response.getWriter();
+		response.setContentType("text/html");
+		obj.println("Hello World");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
