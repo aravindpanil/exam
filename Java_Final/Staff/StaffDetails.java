d2e33552687e3b3fdfe28f79fe3c29384d155c86
Aravind <aravindpanil@gmail.com>
2017-12-20 18:03:37 +0530

    Add Staff Package

---
+++ Java_Final/Staff/StaffDetails.java
@@ -0,0 +1,32 @@
+package Staff;
+import java.util.Scanner;
+
+public class StaffDetails {
+
+		String id, name,desig;
+		String sub1,sub2,sub3;
+		
+		public static void main(String[] args) {
+			StaffDetails obj1 = new StaffDetails();
+			obj1.setDetails();
+			obj1.display();
+		}
+		
+		void setDetails(){
+			System.out.println("Enter id, name and designation\n");
+			Scanner sc = new Scanner(System.in);
+			id=sc.nextLine();
+			name=sc.nextLine();
+			desig=sc.nextLine();
+			System.out.println("Enter three subjects\n");
+			sub1 = sc.nextLine();
+			sub2 = sc.nextLine();
+			sub3 = sc.nextLine();
+			sc.close();
+		}
+		
+		void display(){
+			System.out.println("Name:"+name+"\n"+"Designation:"+desig+"\n"+"Subject 1:"+sub1+"\n"+"Subject 2:"+sub2+"\n"+"Subject 3:"+sub3);
+		}
+}
+
