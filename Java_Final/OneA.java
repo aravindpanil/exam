e79029f958b92baa72a9e325aa76d41a75db129c
Aravind <aravindpanil@gmail.com>
2017-12-20 17:23:09 +0530

    Object Creation for interface

--- Java_Final/OneA.java
+++ Java_Final/OneA.java
@@ -1,16 +1,56 @@
 interface Student{
-	String sname = "";
-	String sno = "";
-	int cls = 0;
 	String getValue();
 }
 
 interface Department{
-	int dno = 0;
 	float getAttendance();
+	int total = 54; //change this value for total classes
 }
 
 interface exam extends Student, Department{
 	void calAttendance();
 	boolean eligible();
+}
+
+public class OneA implements exam{
+	String sname, sno;
+	int cls,attendance;
+	float attper;
+	
+	OneA(String name,String no,int cl,int att){
+		sname = name;
+		sno = no;
+		cls = cl;
+		attendance = att;
+	}
+	
+	public static void main(String[] args){
+		OneA obj = new OneA("John Jamail","1MSCS012",9,12);
+		OneA obj2 = new OneA("Jessica","1MSCS045",4,65);
+		OneA obj3 = new OneA("John Jamail","1MSCS142",10,90);
+		System.out.println(obj.getValue()+obj2.getValue()+obj3.getValue());
+		obj.calAttendance();
+		obj.getAttendance();
+		System.out.println(obj.eligible());
+	}
+	
+	public String getValue(){
+		return("Name:"+sname+"\nNumber:"+sno+"\ncls:"+cls+"\n");
+	}
+	
+	public float getAttendance(){
+		return attper;
+	}
+	
+	public void calAttendance(){
+		attper = attendance/total*100;
+	}
+
+	public boolean eligible(){
+		if(attper >= 85)
+			return true;
+		else
+			return false;
+	}
+	
 }
